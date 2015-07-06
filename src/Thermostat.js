62e9396d6f837fa0fbf4b909f9c49dd45c78ab2b
Makers Laptop 88 <makerslaptop88@Makerss-MacBook-Pro-17.local>
2015-07-06 17:24:57 +0100

    can increase and decrease the temperature

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,3 +1,11 @@
 function Thermostat() {
   this.temp = 20;
 }
+
+Thermostat.prototype.down = function(num) {
+  this.temp -= num;
+};
+
+Thermostat.prototype.up = function(num) {
+  this.temp += num;
+};
